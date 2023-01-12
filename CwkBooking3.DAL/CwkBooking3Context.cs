493c765396bb8cd0025b166996e159497fd788b6
gwalczak27 <gwalczak.27@gmail.com>
2023-01-12 19:31:26 +0100

    added entity framework and configurations

---
+++ CwkBooking3.DAL/CwkBooking3Context.cs
@@ -0,0 +1,22 @@
+﻿using CwkBooking3.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CwkBooking3.DAL
+{
+    public class CwkBooking3Context : DbContext
+    {
+        public CwkBooking3Context()
+        {
+
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+    }
+}
